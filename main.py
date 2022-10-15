0cb1d828a4398657e62b9831630fb6823a84c3f2
myorn <lednovalex@gmail.com>
2022-10-16 02:15:16 +0300

    api fishbone

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 " Эта хуйня для того чтобы хранить пароли. Спасибо наздоровье "
-from fastapi import FastAPI
-from pydantic import BaseModel
 import sqlite3
 from uuid import uuid4
+from fastapi import FastAPI
+from pydantic import BaseModel
+import bcrypt
 
 # db
-con = sqlite3.connect("tutorial.db")
+con = sqlite3.connect("main.db")
 
 
 # init tables
@@ -19,10 +20,45 @@ cur.execute("CREATE TABLE IF NOT EXISTS TOKENS(id, user_id, created_at)")
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    """jhfdsklfjh"""
+@app.post("/auth-me")
+async def auth_me():
+    """ Auth user via login and pass. """
+
+    # create token
+
+    # return token
     return {"message": "хуй"}
+
+
+@app.post("/get-payload/{name}")
+async def get_payload(name: str):
+    """ Get payload by name and auth token. """
+
+    # check if auth token is valid
+
+    # get payload by user_id and payload name
+
+    # return a payload
+    return {"message": "хуй"}
+
+
+@app.post("/push-payload/{name}")
+async def push_payload():
+    """ Push payload by name and auth token. """
+
+    # check if auth token is valid
+
+    # get payload by user_id and payload name
+
+    # return ok
+    return 
+
+
+@app.post("/auth-me")
+async def auth_me():
+    """ Auth user via login and pass. """
+    return {"message": "хуй"}
+
 
 if __name__ == '__main__':
     print('хуй')
