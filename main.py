c4ced0129429785c87a102f07b8b05884fb81522
myorn <lednovalex@gmail.com>
2022-10-16 00:49:14 +0300

    kekeke

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 " Эта хуйня для того чтобы хранить пароли. Спасибо наздоровье "
+from fastapi import FastAPI
+from pydantic import BaseModel
+import sqlite3
+from uuid import uuid4
+
+# db
+con = sqlite3.connect("tutorial.db")
+
+
+# init tables
+cur = con.cursor()
+cur.execute("CREATE TABLE USER(id, name, pwd)")
+cur.execute("CREATE TABLE PAYLOAD(id, user_id, name, value)")
+cur.execute("CREATE TABLE TOKENS(id, user_id, created_at)")
+
+
+# webapp
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    """jhfdsklfjh"""
+    return {"message": "хуй"}
+
+if __name__ == '__main__':
+    print('хуй')
 
